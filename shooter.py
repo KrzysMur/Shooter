c626df310bcd4432e68820a09450d69c9a119fa2
KrzysMur <murek.krzys@gmail.com>
2023-02-26 16:55:47 +0100

    Organize config code into separate file
    New config.py file is added. It contains configparser and variables parsed from config.ini file

--- shooter.py
+++ shooter.py
@@ -2,22 +2,8 @@ import cv2 as cv
 import numpy as np
 import math
 from datetime import datetime
-import configparser
+from config import *
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-caliber = config.getfloat("10m_target", "caliber")
-ex_target_diam = config.getint("10m_target","ex_target_diam")
-ten_diam = config.getint("10m_target","ten_diam")
-ten_x_diam = config.getint("10m_target","ten_x_diam")
-cam_n = config.getint("10m_target","cam")
-min = config.getint("10m_target","min")
-max = config.getint("10m_target","max")
-blur = config.getint("10m_target","blur")
-tenth = round((ex_target_diam / 2 - ten_diam / 2) / 89, 2)   #mm per 0.1
-
-ring_diam = tenth * 10
 
 class Shot:
     def __init__(self, circle):
