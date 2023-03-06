3c6bccf17c2d412bbce3652c0f91cf6abc6158ea
KrzysMur <murek.krzys@gmail.com>
2023-03-06 07:28:37 +0100

    Command based UI
    >>> This is a field for you to enter a Command
    >>> s n - "s" starts a new session. "n" is a number of shots in this session
    >>> help - shows possible commands
    After you start a session you have to highlight the camera window by clicking at it.
    At the end of the session stats automatically shows up.

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ from shooter import *
 def main():
     vid_source_init(cam_n)
     while True:
-        output = frame_process()
-        circle = search_circle(output, min, max)
-        if shot_listener(circle):
-            shot = Shot(circle)
-            shot.get_dist()
-            shot.calc_val()
-            shot.print_shot_stats()
-        draw_circle(circle, output)
-        cv.imshow("Kamerka", output)
+        try:
+            command = Command(input(">>> "))
+            command.execute()
+        except Exception:
+            print("High level exception occured")
+
 
 if __name__ == "__main__":
     main()
--- shooter.py
+++ shooter.py
@@ -8,8 +8,11 @@ from config import *
 class Shot:
     def __init__(self, circle):
         self.timestamp = datetime.now()
+        self.shot_num = None
         self.circle = [int(x) for x in circle]
-
+        self.inner_flag = ""
+        self.score = None
+        self.no_decimal_score = None
 
     def get_dist(self):
         self.dist = int(math.sqrt((screen_center[0] - self.circle[0])**2 + (screen_center[1] - self.circle[1])**2))
@@ -19,16 +22,83 @@ class Shot:
         dist_abs = self.dist / ppmm
         dist_from_ex = cal_ex_target_diam - dist_abs
         if dist_abs <= cal_ten_x_diam/2:
+            self.inner_flag = "X"
+            self.no_decimal_score = 10
             self.score = round(((cal_ten_x_diam/2 - dist_abs) / inner_tenth) / 10 + 10.4, 1)
         elif dist_abs <= cal_ten_diam/2:
+            self.no_decimal_score = 10
             self.score = round(((cal_ten_diam/2 - dist_abs) / outer_tenth) / 10 + 10, 1)
         elif dist_abs <= cal_ex_target_diam/2:
             self.score = round((dist_from_ex / tenth + 10) / 10 - 10, 1)
+            self.no_decimal_score = int((dist_from_ex / tenth + 10) / 10 - 10)
         else:
             self.score = 0
+        self.score = round(self.score, 1)
 
     def print_shot_stats(self):
-        print(f"Score: {self.score}, time: {self.timestamp}")
+        print(f"{self.shot_num}: {self.score}{self.inner_flag}, time: {self.timestamp}")
+
+
+class Session:
+    def __init__(self, num_of_shots):
+        self.num_of_shots = num_of_shots
+        self.num_of_inners = 0
+        self.score = 0
+        self.no_decimal_score = 0
+
+    def new_shot(self, circle, shot_num):
+        shot = Shot(circle)
+        shot.shot_num = shot_num
+        shot.get_dist()
+        shot.calc_val()
+        shot.print_shot_stats()
+        self.score += shot.score
+        self.score = round(self.score, 1)
+        self.no_decimal_score += shot.no_decimal_score
+        if shot.inner_flag == "X":
+            self.num_of_inners += 1
+
+    def session(self):
+        shot_num = 1
+        while shot_num <= self.num_of_shots:
+            output = frame_process()
+            circle = search_circle(output, min, max)
+            if shot_listener(circle):
+                self.new_shot(circle, shot_num)
+                shot_num += 1
+            draw_circle(circle, output)
+            cv.imshow("Kamerka", output)
+        cv.destroyAllWindows()
+
+    def print_session_stats(self):
+        print(f"""
+        Stats of this session
+            Number of shots: {self.num_of_shots}
+            Total score: {self.score}
+            Score without decimal: {self.no_decimal_score}
+            Number of inner tens: {self.num_of_inners}
+            Average shot: {round(self.score / self.num_of_shots, 1)}
+              """)
+
+
+class Command:
+    def __init__(self, command):
+        self.cmd = command
+        self.cmd = self.cmd.split()
+
+    def execute(self):
+        try:
+            match self.cmd[0]:
+                case "s":
+                    session = Session(int(self.cmd[1]))
+                    session.session()
+                    session.print_session_stats()
+                case "help":
+                    print("- 's 10' - s command starts a new session. The number represents the amount of shots in this session")
+                case _:
+                    print("Command doen't exist")
+        except Exception as e:
+            print("Can't execute this command", e)
 
 
 def vid_source_init(cam):
@@ -43,7 +113,6 @@ def vid_source_init(cam):
         print("Can't initialize camera.")
     else:
         print("Camera initialized successfully")
-
 
 def frame_process():
     ret, frame = cap.read()
